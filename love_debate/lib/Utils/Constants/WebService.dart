6db35c336a89559d977dee020a86d10312c5c989
Touseef Sarwar <touseef@yentna.com>
2020-06-11 14:30:53 +0500

    Commit Push..... Network Controllers has been updated. 11 June, 2020

--- love_debate/lib/Utils/Constants/WebService.dart
+++ love_debate/lib/Utils/Constants/WebService.dart
@@ -1,9 +1,11 @@
 class WebService{
-  static var baseURL = 'http://35.182.46.28/Webservice/';
-  static var isRegistered = 'is_registered';
-  static var login = 'login_signup';
-  static var products = 'products_listing';
-  static var modifiers = 'modifiers_listing';
-  static var submitOrder = 'order_submit';
-  static var myOrders = 'my_orders';
+  static var baseURL = "https://lovedebate.co/api/";
+  static var register = "is_registered";
+  static var login = "login";
+
+}
+
+class HttpMethod{
+  static String post = "post";
+  static String get = 'get';
 }
